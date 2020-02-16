734284ab9e5e8f8d6d73abf46b94a83f5607db94
Trev Harmon <trevharmon@gmail.com>
2020-02-15 21:04:34 -0700

    add warning/error defined type
    This should simplify the code elsewhere to handle the showning of
    errors and warnings according to the mdoule setting.

---
+++ manifests/warning.pp
@@ -0,0 +1,16 @@
+define raspberrypi_conifg::warning (
+  String  $message          = $itle,
+  Boolean $show_warning     = $::raspberrypi_config::show_warnings;
+  Boolean $warning_as_error = $::raspberrypi_config::warnings_as_errors,
+) {
+
+  if $warning_as_error_error {
+    fail($message)
+  }
+  else {
+    if $how_warning {
+      notify{ $message: }
+    }
+  }
+
+}
