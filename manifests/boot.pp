c738e4c895195a0306d8a7676992079b0e62705a
Trev Harmon <trevharmon@gmail.com>
2020-02-15 21:32:50 -0700

    add first set of warnings
    This is a first pass at providing some warnings for potentially
    invalid configuration options.

--- manifests/boot.pp
+++ manifests/boot.pp
@@ -6,7 +6,9 @@
 #   include raspberrypi_config
 class raspberrypi_config::boot {
 
-  $start_x = $::raspberrypi_config::start_x ? {
+  $start_file = $::raspberrypi_config::start_file
+  $fixup_file = $::raspberrypi_config::fixup_fil
+  $start_x    = $::raspberrypi_config::start_x ? {
     undef   => undef,
     0       => false,
     1       => true,
@@ -42,13 +44,13 @@ class raspberrypi_config::boot {
     1       => true,
     default => $::raspberrypi_config::kernel_old,
   }
-
   $ramfsfile = $::raspberrypi_config::ramfsfile ? {
     String  => [$::raspberrypi_config::ramfsfile],
     Array   => $::raspberrypi_config::ramfsfile,
     default => undef,
   }
-
+  $ramfsaddr      = $::raspberrypi_config::ramfsaddr
+  $initramfs      = $::raspberrypi_config::initramfs
   $disable_splash = $::raspberrypi_config::disable_splash ? {
     undef   => undef,
     0       => false,
@@ -62,11 +64,32 @@ class raspberrypi_config::boot {
     default => $::raspberrypi_config::enable_gic,
   }
 
+  if ($startup_file == undef and $fixup_file != undef) or 
+     ($startup_file != undef and $fixup_file == undef) {
+    raspberrypi_config::warning{ 'startup_file and fixup_file must either both be defined or undefined': }
+  }
+  if $arm_control {
+    raspberrypi_config::warning{ 'arm_control has been DEPRECATED, use arm_64bit to enable 64-bit kernels': }
+  }
+  if $ramfsaddr != undef and $ramfsfile == undef {
+    raspberrypi_config::warning{ 'When using a ramdisk, both ramfsfile and ramsfaddr should be specified': }
+  }
+  if ($initramfs_file == undef and $initramfs_addr != undef) or 
+     ($initramfs_file != undef and $initramfs_addr == undef) {
+    raspberrypi_config::warning{ 'startup_file and fixup_file must either both be defined or undefined': }
+  }
+  if $ramfsfile != undef and $initramfs_file != undef {
+    raspberrypi_config::warning{ 'A ramdisk should either be defined using ramfsfile/ramfsaddr or initramfs_file/initramfs_addr': }
+  }
+  if $enable_gic and ($::raspberrypi_config::model_type != 'B' or $::raspberrypi_config::model_version != 4) {
+    rapsberrypi_config::waring{ 'The enable_gic feature is only available on Pi 4B modiles': }
+  }
+
   concat::fragment { "${config_file} Boot":
     source  => $::raspberrypi_config::config_file,
     content => epp('raspberrypi_config/boot.epp', {
-      'start_file'               => $::raspberrypi_config::start_file,
-      'fixup_file'               => $::raspberrypi_config::fixup_file,
+      'start_file'               => $start_file,
+      'fixup_file'               => $fixup_file,
       'start_x'                  => $start_x,
       'start_debug'              => $start_debug,
       'disable_commandline_tags' => $disable_commandline_tags,
@@ -77,7 +100,7 @@ class raspberrypi_config::boot {
       'kernel_address'           => $::raspberrypi_config::kernel_address,
       'kernel_old'               => $kernel_old,
       'ramfsfile'                => $ramfsfile,
-      'ramfsaddr'                => $::raspberrypi_config::ramfsaddr,
+      'ramfsaddr'                => $ramfsaddr,
       'initramfs_file'           => $::raspberrypi_config::initramfs_file,
       'initramfs_addr'           => $::raspberrypi_config::initramfs_addr,
       'init_uart_baud'           => $::raspberrypi_config::init_uart_baud,
--- manifests/memory.pp
+++ manifests/memory.pp
@@ -13,13 +13,59 @@ class raspberrypi_config::memory {
     default => $::raspberrypi_config::disablel2cache,
   }
 
+  $gpu_mem      = $::raspberrypi_config::pgu_mem
+  $gpu_mem_256  = $::raspberrypi_config::gpu_mem_256
+  $gpu_mem_512  = $::raspberrypi_config::gpu_mem_512
+  $gpu_mem_1024 = $::raspberrypi_config::gpu_mem_1024
+  $memory       = $facts['raspberry_pi_model']['memory']
+
+  if $gpu_mem != undef {
+    # Check for overridden values
+    if $gpu_mem_256 != undef {
+      raspberrypi_config::warning { 'gpu_mem_256 will override gpu_mem': }
+    } 
+    if $gpu_mem_512 != undef {
+      raspberrypi_config::warning { 'gpu_mem_256 will override gpu_mem': }
+    } 
+    if $gpu_mem_1024 != undef {
+      raspberrypi_config::warning { 'gpu_mem_256 will override gpu_mem': }
+    }
+    # Check for exceeding maximums
+    case $memory {
+      '256 MB': {
+        if $gpu_mem > 192 {
+          raspberrypi_config::warning { 'gpu_mem cannont be above 192 for this model': }
+        }
+      }
+      '1 GB': {
+        if $gpu_mem > 944 {
+          raspberrypi_config::warning { 'gpu_mem cannont be above 944 for this model': }
+        }
+      }
+      default: {
+        if $gpu_mem > 1024 {
+          raspberrypi_config::warning { 'gpu_mem cannont be above 448 for this model': }
+        }
+      }
+    }
+  }
+  elsif $gpu_mem_256 != undef and $memory !~ /256/ {
+    raspberrypi_config::warning{"Cannot use gpu_mem_256 for a model with memory ${memory}": }
+  }
+  elsif $gpu_mem_512 != undef and $memory !~ /512/ {
+    raspberrypi_config::warning{"Cannot use gpu_mem_512 for a model with memory ${memory}": }
+  }
+  elsif $gpu_mem_1024 != undef and $memory != '1 GB' {
+    raspberrypi_config::warning{"Cannot use gpu_mem_1024 for a model with memory ${memory}": }
+  }
+
   concat::fragment { "${config_file} Memory":
     source  => $::raspberrypi_config::config_file,
     content => epp('raspberrypi_config/memory.epp', {
-      'gpu_mem'         => $::raspberrypi_config::pgu_mem,
-      'gpu_mem_256'     => $::raspberrypi_config::gpu_mem_256,
-      'gpu_mem_512'     => $::raspberrypi_config::gpu_mem_512,
-      'gpu_mem_1024'    => $::raspberrypi_config::gpu_mem_1024,
+      'gpu_mem'         => $pgu_mem,
+      'gpu_mem_256'     => $gpu_mem_256,
+      'gpu_mem_512'     => $gpu_mem_512,
+      'gpu_mem_1024'    => $gpu_mem_1024,
       'disable_l2cache' => $disable_l2cache,
     }),
     order   => '100',
